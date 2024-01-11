a21f59c487b809154bf51c7f6f79050f7351b617
connielc1 <constanzalatorrecid@gmail.com>
2024-01-10 21:43:51 -0300

    avance

--- src/News.js
+++ src/News.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 
 const News = () => {
   const [news, setNews] = useState([]);
+  const [categoryFilter, setCategoryFilter] = useState('');
+  const [titleSearch, setTitleSearch] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -11,6 +13,8 @@ const News = () => {
           params: {
             token: '518c4567a749f5b800a5f9ae3b202a18',
             lang: 'es',
+            category: categoryFilter,
+            q: titleSearch,
           },
         });
         setNews(response.data.articles);
@@ -20,20 +24,50 @@ const News = () => {
     };
 
     fetchData();
-  }, []);
+  }, [categoryFilter, titleSearch]);
+
+  const handleCategoryChange = (category) => {
+    setCategoryFilter(category);
+  };
+
+  const handleTitleSearchChange = (event) => {
+    setTitleSearch(event.target.value);
+  };
 
   return (
     <div>
-      <h1>Últimas Noticias</h1>
-      <ul>
+      <h1>Noticias</h1>
+      <div className="filters">
+        <select value={categoryFilter} onChange={(e) => handleCategoryChange(e.target.value)}>
+          <option value="">Todas las categorías</option>
+          <option value="business">Negocios</option>
+          <option value="entertainment">Entretenimiento</option>
+          <option value="health">Salud</option>
+          <option value="science">Ciencia</option>
+          <option value="sports">Deportes</option>
+          <option value="technology">Tecnología</option>
+        </select>
+        <input
+          type="text"
+          placeholder="Buscar por título"
+          value={titleSearch}
+          onChange={handleTitleSearchChange}
+        />
+      </div>
+      <div className="container">
         {news.map((article) => (
-          <li key={article.url}>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">
-              {article.title}
-            </a>
-          </li>
+          <div key={article.url} className="news-box">
+            <h2>
+              <a href={article.url} target="_blank" rel="noopener noreferrer">
+                {article.title}
+              </a>
+            </h2>
+            <p>{article.description}</p>
+            <img className="news-image" src={article.image} alt={article.title} />
+            <p>Fuente: <a href={article.source.url} target="_blank" rel="noopener noreferrer">{article.source.name}</a></p>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
